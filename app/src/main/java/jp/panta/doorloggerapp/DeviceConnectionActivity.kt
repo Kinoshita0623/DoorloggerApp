516d5067968cf705e0e46979862a90b063f866bb
kinoshita0623 <kuutarousii@gmail.com>
2021-04-28 12:27:49 +0900

    Queryを受け取れるようにした

--- app/src/main/java/jp/panta/doorloggerapp/DeviceConnectionActivity.kt
+++ app/src/main/java/jp/panta/doorloggerapp/DeviceConnectionActivity.kt
@@ -2,10 +2,18 @@ package jp.panta.doorloggerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import jp.panta.doorloggerapp.databinding.ActivityDeviceConnectionBinding
 
 class DeviceConnectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device_connection)
+
+        val binding = DataBindingUtil.setContentView<ActivityDeviceConnectionBinding>(this, R.layout.activity_device_connection)
+        binding.deviceTokenView.text = intent.data.toString()
+        binding.deviceTokenView.text = intent.data?.getQueryParameter("token")
+
+
     }
 }
