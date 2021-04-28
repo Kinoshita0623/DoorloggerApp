f25767ed4a149a3dec6983e0cd7974e78dfe117c
kinoshita0623 <kuutarousii@gmail.com>
2021-04-28 14:45:11 +0900

    リストの要素を作成した

--- app/src/main/java/jp/panta/doorloggerapp/DeviceConnectionActivity.kt
+++ app/src/main/java/jp/panta/doorloggerapp/DeviceConnectionActivity.kt
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import jp.panta.doorloggerapp.databinding.ActivityDeviceConnectionBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -11,21 +12,26 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.mapNotNull
 
 class DeviceConnectionActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDeviceConnectionBinding
+
     @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device_connection)
 
         val binding = DataBindingUtil.setContentView<ActivityDeviceConnectionBinding>(this, R.layout.activity_device_connection)
         binding.deviceTokenView.text = intent.data.toString()
         binding.deviceTokenView.text = intent.data?.getQueryParameter("token")
 
+        val deviceListViewModel = ViewModelProvider(this, DeviceListViewModel.Factory())[DeviceListViewModel::class.java]
 
         val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
         lifecycleScope.launchWhenStarted {
             this@DeviceConnectionActivity.registerDeviceFoundBroadcastReceiver().mapNotNull {
-
+                it as? Event.DeviceFound
+            }.collect {
+                deviceListViewModel.addDevice(it.device)
             }
         }
 
